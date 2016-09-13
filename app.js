a72d3a6c4907710412f0a5ba14156031b95e7631
Steve Witman <stevewitman@gmail.com>
2016-09-13 01:11:47 -0600

    connect api to Mongo database

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 
 var app = express();
 
@@ -8,8 +13,13 @@ var bookRouter = express.Router();
 
 bookRouter.route('/books')
   .get(function(req, res) {
-    var responseJSON = {hello: 'This is my API'};
-    res.json(responseJSON)
+    Book.find(function(err, books) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(books);
+      }
+    });
   });
 
 app.use('/api', bookRouter)
