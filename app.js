2bb2c17ca0778c773ff0fbca76395a877a28345f
Steve Witman <stevewitman@gmail.com>
2016-09-12 23:41:02 -0600

    set up a book router

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+  .get(function(req, res) {
+    var responseJSON = {hello: 'This is my API'};
+    res.json(responseJSON)
+  });
+
+app.use('/api', bookRouter)
+
+
 app.get('/', function(req, res) {
   res.send('welcome to my API');
 });
